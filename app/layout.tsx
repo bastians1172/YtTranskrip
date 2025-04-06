c212783892f58527db3bef1938c9c0d38dd98250
Bastian <bastins321@gmail.com>
2025-04-06 12:09:42 +0800

    megubahgoogle analityc

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,22 +51,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-<head>
-      {/* Google Tag Manager */}
-      <Script id="gtm-script" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){
-            w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5DZDVLRV');
-        `}
-      </Script>
-      {/* End Google Tag Manager */}
-</head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased ` }
       >
@@ -80,11 +64,16 @@ export default function RootLayout({
             {children}
             <Footer/>
           </ThemeProvider>
-{/* Google Tag Manager (noscript) */}
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DZDVLRV"
-  height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
-{/* End Google Tag Manager (noscript) */}
       </body>
-    </html>
+  <Script src="https://www.googletagmanager.com/gtag/js?id=G-ZYVBYHBKR2" />
+  <Script id="google-analytics">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-ZYVBYHBKR2');
+    `}
+  </Script>
+</html>
   );
 }
